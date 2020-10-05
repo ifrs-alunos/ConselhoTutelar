ceb6f328643b2f288ce2a89a4f45252d42e60cd7
Guilherme Alves <ghstr@users.noreply.github.com>
2020-10-05 16:32:04 -0300

    adicionando Models

--- esqueleto/app/admin.py
+++ esqueleto/app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(Secretaria) 
+admin.site.register(Conselheiro) 
+admin.site.register(Arquivo)
+admin.site.register(Bairro)
+admin.site.register(Cidade)
+admin.site.register(Endereco)
+admin.site.register(Contato)
+admin.site.register(Comunicante)
+admin.site.register(Anotacao)
+admin.site.register(DireitoViolado)
+admin.site.register(Escola)
+admin.site.register(Denuncia)
+admin.site.register(Ocorrencia)
+admin.site.register(Vitima)
+
+
+
+
