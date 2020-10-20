5518166e0d2ad56c808e616d3046d134494a5913
Guilherme Alves <ghstr@users.noreply.github.com>
2020-10-20 01:31:07 -0300

    Update views.py

--- esqueleto/app/views.py
+++ esqueleto/app/views.py
@@ -11,7 +11,7 @@ def secretaria(request):
 def registro_comunicante(request):
     form = ComunicanteForm()
     if request.method == "POST":
-        form = ComunicanteForm(request.POST)
+        form = ComunicanteForm(request.POST,form_endereco)
         if form.is_valid():
             comunicante = form.save()
     contexto = {
