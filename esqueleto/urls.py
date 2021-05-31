7616bcad2141c991bb77c469181ad61739b673e1
Guilherme Alves da Silva <guilherme1077@gmail.com>
2021-05-31 20:01:21 -0300

    Update urls.py

--- esqueleto/urls.py
+++ esqueleto/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sol/',include('app.urls')),
+    path('sol_app/',include('app.urls')),
     path('accounts/',include('accounts.urls'))
 ]
