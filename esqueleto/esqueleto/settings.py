67b0ef05a23605f24e6798edb2a8fd651df0cca0
Guilherme Alves da Silva <guilherme1077@gmail.com>
2021-05-31 19:12:37 -0300

    mudando o static

--- esqueleto/esqueleto/settings.py
+++ esqueleto/esqueleto/settings.py
@@ -121,14 +121,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 LOGIN_URL = '/accounts/log'
 LOGIN_REDIRECT_URL = '/sol'
 
 from django.contrib.messages import constants as messages
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 MESSAGE_TAGS = {
     messages.DEBUG: 'alert-info',
